dafbc764a5fc627a07f68534211d55149f2fad01
gonzalo <greco_gonzalo@hotmail.com>
2019-04-09 16:52:16 -0300

    progreso clase 2

--- Clase2/index.php
+++ Clase2/index.php
@@ -1,11 +1,38 @@
 <?php
     require ".\Clases\Alumno.php";
-    //$_server
     $alumnos = array();
-    $alumno = new Alumno($_GET["nombre"], $_GET["apellido"], $_GET["dni"], $_GET["legajo"]);
-    array_push($alumnos, $alumno);
 
-    var_dump($alumnos);
+    //echo $_server['REQUEST_METHOD']
+    
+    switch (getenv('REQUEST_METHOD')) 
+    {
+        case 'GET':
+            $alumno = new Alumno($_GET["nombre"], $_GET["apellido"], $_GET["dni"], $_GET["legajo"]);
+            array_push($alumnos, $alumno);
+            break;
+
+        case 'POST':
+            # code...
+            break;
+
+        case 'PUT':
+            $archivo = $_PUT['archivo'];
+            $json = json_encode($alumnos);
+            $f = fopen($archivo, 'a');
+            fwrite($f, $json);
+            fclose($f);
+            break;
+
+        case 'DELETE':
+            # code...
+            break;
+                
+        default:
+            echo "Error";
+            break;
+    }
+
+    //var_dump($alumnos);
 
     $f = fopen("data.json", "a");
 	fwrite($f,json_encode($alumnos));
